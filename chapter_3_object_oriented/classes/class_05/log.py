364a4190fce8fcc009473a814bb123f074aae5b0
MangoGab <gabrielhmango@gmail.com>
2023-05-30 18:38:17 -0300

    log.py file update

--- chapter_3_object_oriented/classes/class_05/log.py
+++ chapter_3_object_oriented/classes/class_05/log.py
@@ -1,11 +1,22 @@
 class Log:
-    def log(self, msg):
+    def _log(self, message):
         raise NotImplementedError('Implement the log method!')
     
+    def log_error(self, message):
+        return self._log(f'Error: {message}')
+    
+    def log_success(self, message):
+        return self._log(f'Success: {message}')
+    
 class LogFileMixin(Log):
-    def log(self, msg):
-        print(msg)
+    def _log(self, message):
+        print(message)
+
+class LogPrintMixin(Log):
+    def _log(self, message):
+        print(f'{message} ({self.__class__.__name__})')
 
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('Test method.')
+    l = LogPrintMixin()
+    l.log_error('Test error method.')
+    l.log_success('Test sucess method.')
