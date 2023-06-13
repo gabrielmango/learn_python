a157b1c049207bb418fa54cf52d732427d6b5a78
MangoGab <gabrielhmango@gmail.com>
2023-06-13 18:39:22 -0300

    Creating the CurrentAccount class

--- chapter_3_object_oriented/projects/banking_system/account.py
+++ chapter_3_object_oriented/projects/banking_system/account.py
@@ -1,7 +1,7 @@
 import abc
 
 class Account(abc.ABC):
-    def __init__(self, agency, account, balance):
+    def __init__(self, agency, account, balance=0):
         self.agency = agency
         self.account = account
         self.balance = balance
@@ -27,11 +27,28 @@ class SavingsAccount(Account):
             return self.balance
         
         print('Unable to withdraw desired amount!')
-        self.details(f'(WITHDRAW DENIED) {value}')    
+        self.details(f'(WITHDRAW DENIED) {value}') 
 
+
+class CurrentAccount(Account):
+    def __init__(self, agency, account, balance=0, limit=0):
+        super().__init__(agency, account, balance)
+        self.limit = limit
+
+    def withdraw_money(self, value):
+        value_after_withdrawal = self.balance - value
+        maximum_limit = -self.limit
+
+        if value_after_withdrawal >= maximum_limit:
+            self.balance -= value
+            self.details(f'(WITHDRAW) {value}')
+            return self.balance
+        
+        print('Unable to withdraw desired amount!')
+        print(f'Your limit is {-self.limit:.2f}')
+        self.details(f'(WITHDRAW DENIED) {value}')
 
 if __name__ == '__main__':
-    savings_account_1 = SavingsAccount(111, 222, 0)
-    savings_account_1.withdraw_money(1)
-    savings_account_1.cash_deposit(1)
-    savings_account_1.withdraw_money(1)
+    current_account_1 = CurrentAccount(111, 222, 0, 100)
+    current_account_1.withdraw_money(1)
+    current_account_1.withdraw_money(1)
