d00bf1dd9808fddac72c69c994874bd7c46669b2
MangoGab <gabrielhmango@gmail.com>
2023-06-13 18:24:52 -0300

    Creating the SavingsAccount class

--- chapter_3_object_oriented/projects/banking_system/account.py
+++ chapter_3_object_oriented/projects/banking_system/account.py
@@ -15,3 +15,23 @@ class Account(abc.ABC):
 
     def details(self, message = ''):
         print(f'Your balance is {self.balance:.2f} {message}')
+        print('---')
+
+class SavingsAccount(Account):
+    def withdraw_money(self, value):
+        value_after_withdrawal = self.balance - value
+
+        if value_after_withdrawal >= 0:
+            self.balance -= value
+            self.details(f'(WITHDRAW) {value}')
+            return self.balance
+        
+        print('Unable to withdraw desired amount!')
+        self.details(f'(WITHDRAW DENIED) {value}')    
+
+
+if __name__ == '__main__':
+    savings_account_1 = SavingsAccount(111, 222, 0)
+    savings_account_1.withdraw_money(1)
+    savings_account_1.cash_deposit(1)
+    savings_account_1.withdraw_money(1)
