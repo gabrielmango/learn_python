ed5b56efba493e0ed0b1e06cae2e99029356bac8
MangoGab <gabrielhmango@gmail.com>
2023-06-14 20:01:57 -0300

    Using calendar in python for calendars and dates

---
+++ chapter_4_python_modules/classes/calss_02.py
@@ -0,0 +1,30 @@
+import calendar
+
+import os
+
+os.system('cls')
+
+year_2022 = calendar.calendar(2022)
+print(year_2022)
+
+print('-' * 100)
+
+dezember_2022 = calendar.month(2022, 12)
+print(dezember_2022)
+
+print('-' * 100)
+
+last_day_dezember_2022 = calendar.monthrange(2022, 12)
+print(last_day_dezember_2022)
+print(list(enumerate(calendar.day_name)))
+
+day_number, last_day = calendar.monthrange(2022, 12)
+print(calendar.day_name[day_number])
+
+print('-' * 100)
+
+calendar_dezember_2022 = calendar.monthcalendar(2012, 12)
+print(calendar_dezember_2022)
+
+for week in calendar_dezember_2022:
+    print(week)
